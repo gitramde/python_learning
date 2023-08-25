7953f62bc5c0d410dec1b6091e75aa2cb46d7aa1
Ram S <ramkumars2007@gmail.com>
2023-08-25 08:44:43 -0400

    Contains Duplicate Solution

--- leekcode_Blind75/P217_ContainsDuplicate.py
+++ leekcode_Blind75/P217_ContainsDuplicate.py
@@ -1,8 +1,14 @@
-# Given an integer array nums, return true if any value appears at least twice
+# Given an integer array nums, return true if any value appears AT LEAST twice
 # in the array, and return false, if every element is distinct.
 
 # Example:
 #  [1,2,3,1] --> true
 #  [1,2,3,4] --> false
 
-class
+class ContainsDuplicate:
+   def has_duplicate(self, nums: list[int]) -> bool:
+        hashset = set(nums)
+        if len(hashset) == len(nums):
+            return False
+        else:
+            return True
