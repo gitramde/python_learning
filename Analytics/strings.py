eb41f2d35e2ccef50b8a08dfb5ed7c99507c107a
Ram S <ramkumars2007@gmail.com>
2024-10-26 17:06:48 -0400

    Updates to Strings

--- Analytics/strings.py
+++ Analytics/strings.py
@@ -14,6 +14,8 @@
                 i. str(object) returns printable string representation of the object [type(object).__str__(object)]
                 ii. If object does not have __str__() method then str() falls back to returning repr(object) - a string
                     containing a printable representation of an object.
+                iii. repr() meant to generate representations which can be read by the interpreter. For objects which
+                    don’t have a particular representation for human consumption, str() will return the same value as repr()
             b. If at least one of encoding or errors is given, object should be bytes-like object (bytes or bytearray)
                 i. More details when learning about the bytes and bytearray
 
@@ -42,6 +44,8 @@
                                       'r' - string using repr(), 's' - string using str(), 'a' - string using ascii
                 NOTE: width and length can display any number of characters above the specified value (minimum characters)
         2. formatted string strings (f string)
+            a. Begin with a f or F
+            b. Write a Python expression between { and } characters that can refer to variables or literal values.
         3. str.format()
         4. template_strings 
     
@@ -58,8 +62,7 @@ print (" Grocery Cost : %5.2f;  Number of Items: %5d" % (258.456, 800))
 #  Grocery Cost : 258.46;  Number of Items:   800
 # %5.2f - String need to have minimum 5 digits and 2 decimal points. if not, left padded with spaces
 
-# ----------------------- Formatted Strings - f string ---------------------------- #
-
+# ----------------------- Formatted String Literals - f string ---------------------------- #
 
 
 
